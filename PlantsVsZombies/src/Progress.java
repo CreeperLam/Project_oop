7393cf88affcf48ae0587691527e6684593083da
tqanhah <tqanh6203tkqn@gmail.com>
2024-06-08 21:39:35 +0700

    update reset game

---
+++ PlantsVsZombies/src/Progress.java
@@ -0,0 +1,40 @@
+import javax.swing.JOptionPane;
+
+public class Progress {
+    private static int progress = 143;
+    private static String message = "Level Completed !!!" + '\n' + "Starting next Level";
+    public static void updateProgress(int num) {
+        progress = progress + num;
+        System.out.println(progress);
+        if(progress>=150) {
+           if("1".equals(LevelData.Lvl)) {
+        	   JOptionPane.showMessageDialog(null,message);
+               GameWindow.gw.getYard().reset();
+        	   GameWindow.gw.dispose();
+        	   GameWindow.gw = new GameWindow();
+        	   LevelData.write("2");
+            }
+           else if ("2".equals(LevelData.Lvl)){
+        	   JOptionPane.showMessageDialog(null,message);
+               GameWindow.gw.getYard().reset();
+               GameWindow.gw.dispose();
+               GameWindow.gw = new GameWindow();  
+               LevelData.write("3");
+           }
+           else {
+                LevelData.write("1");
+                JOptionPane.showOptionDialog(null, message, "Level Completed !!!" + '\n' + "More Levels will come soon !!!" + '\n' + "Resetting data", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, new Object[]{"Quit"}, "Quit");
+                System.exit(0);
+           }
+           progress = 80;
+        }
+    }
+
+	public static int getProgress() {
+		return progress;
+	}
+
+	public static void setProgress(int progress) {
+		Progress.progress = progress;
+	}
+}
